31c8ef5a5b55d1a0340952f0eed61ce7104110ba
Konstantin <simussev.k@icloud.com>
2022-06-26 18:44:03 +0500

    Creating a model

---
+++ PersonalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Konstantin Simusev on 24.06.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+}
+
+
