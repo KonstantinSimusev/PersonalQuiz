b0b7ecb7322e845d982d9e56a40629af94eeab94
Konstantin <simussev.k@icloud.com>
2022-06-28 09:28:39 +0500

    Work with logic version 1.02

---
+++ PersonalQuiz/Extensions/Extension + UIView.swift
@@ -0,0 +1,21 @@
+//
+//  Extension + UIView.swift
+//  PersonalQuiz
+//
+//  Created by Konstantin Simusev on 28.06.2022.
+//
+
+import UIKit
+
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+}
